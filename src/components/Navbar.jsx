9e9acb72b5e4c2185280cce68c4d82a11c372e03
Hatala Mark <hatala.mark@gmail.com>
2023-06-13 12:05:39 +0200

    createlistings page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ function Navbar() {
                   : 'navbarListItemName'
               }
             >
-              Offer
+              Offers
             </p>
           </li>
 
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -14,7 +14,7 @@ import { toast } from 'react-toastify'
 import Spinner from '../components/Spinner'
 import ListingItem from '../components/ListingItem'
 
-function Offers() {
+function Category() {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
 
@@ -28,7 +28,7 @@ function Offers() {
         // Create query
         const q = query(
           listingsRef,
-          where('offer', '==', true),
+          where('type', '==', params.categoryName),
           orderBy('timestamp', 'desc'),
           limit(10)
         )
@@ -53,13 +53,15 @@ function Offers() {
     }
 
     fetchListings()
-  }, [])
+  }, [params.categoryName])
 
   return (
     <div className='category'>
       <header>
         <p className='pageHeader'>
-         Offers
+          {params.categoryName === 'rent'
+            ? 'Places for rent'
+            : 'Places for sale'}{' '}
         </p>
       </header>
 
@@ -76,10 +78,10 @@ function Offers() {
           </main>
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>No listings for {params.categoryName}</p>
       )}
     </div>
   )
 }
 
-export default Offers
+export default Category
